9354350756e0b42c15f7c73968d9e94ec7720f0c
Isuu1 <jakub.babelek@gmail.com>
2025-04-20 00:13:22 +0100

    Added TypingAnimation component

--- features/headline/components/Headline.tsx
+++ features/headline/components/Headline.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 //Styles
 import styles from "@/features/headline/components/Headline.module.css";
+import TypingAnimation from "./TypingAnimation";
 
 const Headline = () => {
   return (
     <div className={styles.headlineContainer}>
-      <div>
-        <h2 className="home-container__headline__text">
-          Hello there, my name is Jakub. I`m a <br></br>{" "}
-          {/* <TypeAnimation
+      <h2 className="home-container__headline__text">
+        Hello there, my name is Jakub. I`m a <br></br> <TypingAnimation />
+        {/* <TypeAnimation
             sequence={[
               "front-end developer.",
               1500,
@@ -22,22 +22,21 @@ const Headline = () => {
             repeat={Infinity}
             style={{ color: "#811ae5", fontWeight: "bold" }}
           /> */}
-        </h2>
+      </h2>
 
-        <div className="home-container__headline__buttons">
-          <a href="#projects-container">
-            <button className="home-container__headline__buttons__my-work">
-              My work
-              {/* <TbArrowBigDownLineFilled className="icon" /> */}
-            </button>
-          </a>
-          <a href="https://github.com/Isuu1">
-            <button className="home-container__headline__buttons__github">
-              GitHub
-              {/* <FaGithub className="icon" /> */}
-            </button>
-          </a>
-        </div>
+      <div className="home-container__headline__buttons">
+        <a href="#projects-container">
+          <button className="home-container__headline__buttons__my-work">
+            My work
+            {/* <TbArrowBigDownLineFilled className="icon" /> */}
+          </button>
+        </a>
+        <a href="https://github.com/Isuu1">
+          <button className="home-container__headline__buttons__github">
+            GitHub
+            {/* <FaGithub className="icon" /> */}
+          </button>
+        </a>
       </div>
     </div>
   );
