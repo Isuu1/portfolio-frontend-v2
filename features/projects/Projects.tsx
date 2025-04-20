d48c7ad0d915f75db11c15ed5b5e81219566def8
Isuu1 <jakub.babelek@gmail.com>
2025-04-20 02:10:21 +0100

    Style Projects

--- features/projects/Projects.tsx
+++ features/projects/Projects.tsx
@@ -6,7 +6,7 @@ import React from "react";
 import styles from "@/features/projects/Projects.module.css";
 
 //Animations
-import { motion, AnimatePresence } from "motion/react";
+import { motion } from "motion/react";
 import CategoryMenu from "./CategoryMenu";
 
 const Projects = () => {
@@ -22,7 +22,7 @@ const Projects = () => {
       <div className="shape-blob one"></div>
       <div className="shape-blob two"></div>
       <motion.div
-        className="projects-container__headline"
+        className={styles.headline}
         whileInView={"visible"}
         initial="hidden"
         // variants={headlineSpringAnimation}
@@ -30,48 +30,6 @@ const Projects = () => {
         My work
       </motion.div>
       <CategoryMenu />
-      {/* <motion.div
-        className="projects-container__type-selector"
-        whileInView={"visible"}
-        initial="hidden"
-        variants={childDelayTopSpring}
-      >
-        <motion.button
-          onClick={() => handleCatChange("All")}
-          className={`projects-container__type-selector__button ${
-            typeFilter === "All" ? "selected" : null // Adding class 'selected' to the button
-          }`}
-          variants={opacityAnimation}
-        >
-          All
-        </motion.button>
-        <motion.button
-          onClick={() => handleCatChange("Web")}
-          className={`projects-container__type-selector__button ${
-            typeFilter === "Web" ? "selected" : ""
-          }`}
-          variants={opacityAnimation}
-        >
-          Web
-        </motion.button>
-        <motion.button
-          onClick={() => handleCatChange("Ecommerce")}
-          className={`projects-container__type-selector__button ${
-            typeFilter === "Ecommerce" ? "selected" : ""
-          }`}
-          variants={opacityAnimation}
-        >
-          Ecommerce
-        </motion.button>
-        <motion.button
-          onClick={() => handleCatChange("Mobile")}
-          className="projects-container__type-selector__button"
-          variants={opacityAnimation}
-        >
-          Mobile
-        </motion.button>
-      </motion.div> */}
-      <AnimatePresence mode="wait"></AnimatePresence>
     </motion.div>
   );
 };
