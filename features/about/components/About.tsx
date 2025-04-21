5896d30dbaf3596770d273fb28075adde4919f47
Isuu1 <jakub.babelek@gmail.com>
2025-04-21 01:30:07 +0100

    Add animation for about me info

--- features/about/components/About.tsx
+++ features/about/components/About.tsx
@@ -6,6 +6,31 @@ import React from "react";
 import styles from "@/features/about/components/About.module.scss";
 import Image from "next/image";
 import SectionHeadline from "@/shared/components/SectionHeadline";
+//Animations
+import { motion } from "motion/react";
+
+export const aboutMeVariants = {
+  visible: {
+    transition: {
+      delayChildren: 0.2,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const aboutMeItemsVariants = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.3,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    y: -20,
+  },
+};
 
 const About = () => {
   return (
@@ -18,12 +43,17 @@ const About = () => {
           fill
           className={styles.image}
         />
-        <div className={styles.aboutMe}>
-          <p>
+        <motion.div
+          className={styles.aboutMe}
+          initial="hidden"
+          whileInView="visible"
+          variants={aboutMeVariants}
+        >
+          <motion.p variants={aboutMeItemsVariants}>
             Hi, I`m Jakub, an aspiring Full-Stack Software Engineer with a deep
             passion for technology and coding.
-          </p>
-          <p>
+          </motion.p>
+          <motion.p variants={aboutMeItemsVariants}>
             I transitioned into this field because I thrive on constant
             learning, solving real-world problems, and embracing new challenges.
             I`m currently mastering Next.js and TypeScript, driven by the
@@ -32,9 +62,11 @@ const About = () => {
             skills, which I love applying to complex coding challenges. When I`m
             not immersed in code, you can usually find me out on a bike trip,
             recharging for the next big project.
-          </p>
-          <h3>Get in touch and let`s bring your ideas to life!</h3>
-        </div>
+          </motion.p>
+          <motion.h3 variants={aboutMeItemsVariants}>
+            Get in touch and let`s bring your ideas to life!
+          </motion.h3>
+        </motion.div>
       </div>
     </div>
   );
