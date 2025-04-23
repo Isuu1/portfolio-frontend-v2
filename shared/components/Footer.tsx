35646897d514cd82f8a2463b0a463ce2acac53a0
Isuu1 <jakub.babelek@gmail.com>
2025-04-23 19:13:34 +0100

    Add Footer component

---
+++ shared/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+//Styles
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      Copyright © 2023 Jakub Babelek. All rights reserved.
+    </div>
+  );
+};
+
+export default Footer;
