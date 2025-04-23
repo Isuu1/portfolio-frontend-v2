98f9d046c5c093bb6b7186633346d86ffe7a9992
Isuu1 <jakub.babelek@gmail.com>
2025-04-23 19:54:37 +0100

    Change Headline background + add link to projects

--- features/headline/components/Headline.tsx
+++ features/headline/components/Headline.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import React from "react";
 
 //Styles
-import styles from "@/features/headline/components/Headline.module.css";
+import styles from "@/features/headline/components/Headline.module.scss";
 
 //Components
 import TypingAnimation from "./TypingAnimation";
@@ -14,18 +16,28 @@ import Button from "@/shared/components/ui/Button";
 import Link from "next/link";
 
 const Headline = () => {
+  const scrollToSection = (id: string) => {
+    const el = document.getElementById(id);
+    console.log("scrollToSection", id);
+    if (el) {
+      el.scrollIntoView({ behavior: "smooth" });
+      // optionally remove the hash from the URL
+      history.replaceState(null, "", location.pathname);
+    }
+  };
+
   return (
     <div className={styles.headlineContainer}>
       <h1>Hello there, my name is Jakub. I`m a</h1>
       <TypingAnimation />
       <div className={styles.buttons}>
-        <Link href="#projects-container">
-          <Button
-            variant="secondary"
-            text="My work"
-            icon={<TbArrowBigDownLineFilled />}
-          />
-        </Link>
+        <Button
+          variant="secondary"
+          filled={false}
+          text="My work"
+          icon={<TbArrowBigDownLineFilled />}
+          onClick={() => scrollToSection("projects")}
+        />
         <Link href="https://github.com/Isuu1">
           <Button variant="primary" text="GitHub" icon={<FaGithub />} />
         </Link>
